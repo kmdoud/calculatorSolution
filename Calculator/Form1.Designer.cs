c95ddda90a9c8300e212c55c1436d455acbb5d95
Kenneth M Doud <kmdoud@gmail.com>
2019-02-26 09:20:04 -0500

    initial commit

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -28,13 +28,109 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.minus = new System.Windows.Forms.Button();
+            this.plus = new System.Windows.Forms.Button();
+            this.multiply = new System.Windows.Forms.Button();
+            this.divide = new System.Windows.Forms.Button();
+            this.input1 = new System.Windows.Forms.TextBox();
+            this.input2 = new System.Windows.Forms.TextBox();
+            this.total = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // minus
+            // 
+            this.minus.Location = new System.Drawing.Point(299, 146);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(75, 112);
+            this.minus.TabIndex = 0;
+            this.minus.Text = "-";
+            this.minus.UseVisualStyleBackColor = true;
+            this.minus.Click += new System.EventHandler(this.minus_Click);
+            // 
+            // plus
+            // 
+            this.plus.Location = new System.Drawing.Point(186, 146);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(75, 116);
+            this.plus.TabIndex = 1;
+            this.plus.Text = "+";
+            this.plus.UseVisualStyleBackColor = true;
+            this.plus.Click += new System.EventHandler(this.plus_Click);
+            // 
+            // multiply
+            // 
+            this.multiply.Location = new System.Drawing.Point(186, 287);
+            this.multiply.Name = "multiply";
+            this.multiply.Size = new System.Drawing.Size(75, 147);
+            this.multiply.TabIndex = 2;
+            this.multiply.Text = "*";
+            this.multiply.UseVisualStyleBackColor = true;
+            this.multiply.Click += new System.EventHandler(this.multiply_Click);
+            // 
+            // divide
+            // 
+            this.divide.Location = new System.Drawing.Point(299, 287);
+            this.divide.Name = "divide";
+            this.divide.Size = new System.Drawing.Size(75, 147);
+            this.divide.TabIndex = 3;
+            this.divide.Text = "/";
+            this.divide.UseVisualStyleBackColor = true;
+            this.divide.Click += new System.EventHandler(this.divide_Click);
+            // 
+            // input1
+            // 
+            this.input1.Location = new System.Drawing.Point(33, 287);
+            this.input1.Name = "input1";
+            this.input1.Size = new System.Drawing.Size(100, 44);
+            this.input1.TabIndex = 4;
+            this.input1.Text = "1";
+            this.input1.TextChanged += new System.EventHandler(this.input1_TextChanged);
+            // 
+            // input2
+            // 
+            this.input2.Location = new System.Drawing.Point(446, 259);
+            this.input2.Name = "input2";
+            this.input2.Size = new System.Drawing.Size(100, 44);
+            this.input2.TabIndex = 5;
+            this.input2.Text = "2";
+            this.input2.TextChanged += new System.EventHandler(this.input2_TextChanged);
+            // 
+            // total
+            // 
+            this.total.AutoSize = true;
+            this.total.Location = new System.Drawing.Point(624, 262);
+            this.total.Name = "total";
+            this.total.Size = new System.Drawing.Size(0, 37);
+            this.total.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1347, 740);
+            this.Controls.Add(this.total);
+            this.Controls.Add(this.input2);
+            this.Controls.Add(this.input1);
+            this.Controls.Add(this.divide);
+            this.Controls.Add(this.multiply);
+            this.Controls.Add(this.plus);
+            this.Controls.Add(this.minus);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button minus;
+        private System.Windows.Forms.Button plus;
+        private System.Windows.Forms.Button multiply;
+        private System.Windows.Forms.Button divide;
+        private System.Windows.Forms.TextBox input1;
+        private System.Windows.Forms.TextBox input2;
+        private System.Windows.Forms.Label total;
     }
 }
 
--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -12,9 +12,43 @@ namespace Calculator
 {
     public partial class Form1 : Form
     {
+        
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void input1_TextChanged(object sender, EventArgs e)
+        {
+            var userInput = input1.Text;
+            var userresult1 = int.Parse(userInput);
+        }
+
+        private void input2_TextChanged(object sender, EventArgs e)
+        {
+            var userInput = input1.Text;
+            var userresult = int.Parse(userInput);
+        }
+
+        private void plus_Click(object sender, EventArgs e)
+        {
+            total.Text = (int.Parse(input1.Text) + int.Parse(input2.Text)).ToString();
+        }
+
+        private void minus_Click(object sender, EventArgs e)
+        {
+            total.Text = (int.Parse(input1.Text) - int.Parse(input2.Text)).ToString();
+        }
+
+        private void multiply_Click(object sender, EventArgs e)
+        {
+            total.Text = (int.Parse(input1.Text) * int.Parse(input2.Text)).ToString();
+        }
+
+        private void divide_Click(object sender, EventArgs e)
+        {
+            total.Text = (int.Parse(input1.Text) / int.Parse(input2.Text)).ToString();
         }
     }
 }
